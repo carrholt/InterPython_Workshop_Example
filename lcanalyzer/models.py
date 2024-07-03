3b61a259bb91ecb4d877c24e64604ece74486013
Carrie Holt <cholt@lco.global>
2024-07-03 08:06:23 -0700

    added more tests after looking at debugging strategies

--- lcanalyzer/models.py
+++ lcanalyzer/models.py
@@ -42,3 +42,30 @@ def min_mag(data,mag_col):
     :returns: The min value of the column.
     """
     return data[mag_col].min()
+
+def calc_stats(lc, bands, mag_col):
+    """Calculate max, mean and min values for all bands of a lightcurve.
+    :param lc: pd.DataFrame with observed magnitudes for a single source.
+    :param bands: a list of all magnitude bands.
+    :param mag_col: a string with the name of the column for calculating the min value.
+    :returns: The max, mean, and min values of the column for all bands.
+    """
+    stats = {}
+    for b in bands:
+        stat = {}
+        stat["max"] = max_mag(lc[b], mag_col)
+        stat["mean"] = mean_mag(lc[b], mag_col)
+        stat["min"] = min_mag(lc[b], mag_col)
+        stats[b] = stat
+    return pd.DataFrame.from_records(stats)
+
+def normalize_lc(df,mag_col):
+    """ Normalize a single lightcurve.
+    :param df: pd.DataFrame with observed magnitudes for a single source.
+    :param mag_col: a string with the name of the column for normalizing.
+    :returns: Normalized lightcurve.
+    """
+    min = min_mag(df,mag_col)
+    max = max_mag((df-min),mag_col)
+    lc = (df[mag_col]-min)/max
+    return lc
